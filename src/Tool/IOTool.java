192b50745f2b3efa6c38d920e9a824b26c5378f2
AIR <3545210791@qq.com>
2023-08-18 09:28:18 +0800

    update

--- src/Tool/IOTool.java
+++ src/Tool/IOTool.java
@@ -11,16 +11,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class IOTool {
-    // 全局工具
-    private final FileChooser fileChooser = new FileChooser();
-
-    public IOTool() {
-        this.fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Sudoku Source File (*.sudoku)", "*.sudoku"));
-    }
-
-    public void input(List<List<Label>> confirm_list, List<List<Label>> unknown_list) {
+    public static void input(List<List<Label>> confirm_list, List<List<Label>> unknown_list) {
+        // initialize FileChooser
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Sudoku Source File (*.sudoku)", "*.sudoku"));
+        
         try {
-            File file = this.fileChooser.showOpenDialog(new Stage());
+            File file = chooser.showOpenDialog(new Stage());
 
             if (!(file == null)) {
                 Scanner sc = new Scanner(file);
@@ -32,10 +29,13 @@ public class IOTool {
                         confirm_list.get(i).get(j).setText("");
                         unknown_list.get(i).get(j).setText("");
 
-                        // 写入Label
+                        // 写入Label，分unknown、confirm、未填写进行写入
                         String temp = temps[j];
-                        if (temp.length() == 1 && !temp.equals("0")) confirm_list.get(i).get(j).setText(temp);
-                        else if (!temp.equals("0")) unknown_list.get(i).get(j).setText(temp);
+                        if (temp.length() == 1 && !temp.equals("0")) {
+                            confirm_list.get(i).get(j).setText(temp);
+                        } else if (!temp.equals("0")) {
+                            unknown_list.get(i).get(j).setText(temp);
+                        }
                     }
                 }
             }
@@ -44,28 +44,36 @@ public class IOTool {
         }
     }
 
-    public void output(List<List<Label>> confirm_list, List<List<Label>> unknown_list) {
+    public static void output(List<List<Label>> confirm_list, List<List<Label>> unknown_list) {
         try {
-            File file = this.fileChooser.showSaveDialog(new Stage());
+            // initialize FileChooser
+            FileChooser chooser = new FileChooser();
+            chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Sudoku Source File (*.sudoku)", "*.sudoku"));
+
+            File file = chooser.showSaveDialog(new Stage());
             if (!(file == null)) {
                 file.createNewFile();
 
-                StringBuilder append_str = new StringBuilder();
+                FileWriter writer = new FileWriter(file);
                 for (int i = 0; i < 9; i++) {
                     for (int j = 0; j < 9; j++) {
-                        String confirm_get = confirm_list.get(i).get(j).getText();
-                        String unknown_get = unknown_list.get(i).get(j).getText().replace("\n", "");
+                        String confirm_str = confirm_list.get(i).get(j).getText();
+                        String unknown_str = unknown_list.get(i).get(j).getText().replace("\n", "");
 
-                        if (confirm_get.equals("") && unknown_get.equals("")) append_str.append("0");
-                        else append_str.append(confirm_get).append(unknown_get);
-                        if (j != 8) append_str.append("\0");
+                        if (confirm_str.equals("") && unknown_str.equals("")) {
+                            writer.append("0");
+                        } else {
+                            // 因为confirm_Str和unknown_str只有一个可能有数据，所以并排存储
+                            writer.append(confirm_str).append(unknown_str);
+                        }
+
+                        writer.append("\0");
                     }
-                    if (i != 8) append_str.append("\n");
+
+                    writer.append("\n");
                 }
 
-                FileWriter fileWriter = new FileWriter(file);
-                fileWriter.append(append_str);
-                fileWriter.close();
+                writer.close();
             }
         } catch (Exception e) {
             WinTool.CreateAlert(Alert.AlertType.ERROR, "错误", "写入错误", "");
